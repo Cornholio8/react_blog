e2d76d83b7b1cd6e703d6fba2f5f9d071e54affb
Piotr Kamecki <piotrkamecki8@gmail.com>
2023-01-06 23:45:23 +0100

    add NavBar and Footer

--- src/App.js
+++ src/App.js
@@ -5,18 +5,25 @@ import Home from './components/pages/Home/Home.js';
 import NotFound from './components/pages/NotFound/NotFound.js';
 import Post from './components/pages/Post/Post.js';
 import { Routes, Route } from 'react-router-dom';
+import Container from 'react-bootstrap/Container';
+import Header from './components/views/Header';
+import Footer from './components/views/Footer';
 
 const App = () => {
   return (
     <main>
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/about' element={<About />} />
-        <Route path='/post/:id' element={<Post />} />
-        <Route path='/post/add' element={<AddPost />} />
-        <Route path='/post/edit/:id' element={<EditPost />} />
-        <Route path='*' element={<NotFound />} />
-      </Routes>
+      <Container>
+        <Header />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/about' element={<About />} />
+          <Route path='/post/:id' element={<Post />} />
+          <Route path='/post/add' element={<AddPost />} />
+          <Route path='/post/edit/:id' element={<EditPost />} />
+          <Route path='*' element={<NotFound />} />
+        </Routes>
+        <Footer />
+      </Container>
     </main>
   );
 };
