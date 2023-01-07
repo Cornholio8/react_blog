69d8436171c2f2ffc9b798d1ad012175f9d1397f
Piotr Kamecki <piotrkamecki8@gmail.com>
2023-01-07 18:23:34 +0100

    add home page

--- src/components/pages/Post/Post.js
+++ src/components/pages/Post/Post.js
@@ -1,9 +1,10 @@
 const Post = () => {
-    return (
-      <div>
-        <h1>Post page</h1>
-      </div>
-    );
-  }
+
+  return (
+    <div>
+      <h1>Single post</h1>
+    </div>
+  );
+};
   
   export default Post;
--- src/redux/initialState.js
+++ src/redux/initialState.js
@@ -1,5 +1,32 @@
 const initialState = {
-    posts : [],
+    posts : [
+        {
+            id: '1',
+            title: 'Article 1',
+            shortDescription: 'Short description of the article...',
+            content: 'Main content of the article',
+            publishedDate: '02-02-2022',
+            author: 'John Doe'
+      },
+
+      {
+        id: '2',
+        title: 'Article 2',
+        shortDescription: 'Short description of the article...',
+        content: 'Main content of the article',
+        publishedDate: '02-02-2022',
+        author: 'John Doe'
+  },
+
+  {
+    id: '3',
+    title: 'Article 3',
+    shortDescription: 'Short description of the article...',
+    content: 'Main content of the article',
+    publishedDate: '02-02-2022',
+    author: 'John Doe'
+},
+    ],
 
 };
 
